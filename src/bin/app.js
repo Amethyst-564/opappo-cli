d064cadeed7837892e5a55b457a360300864b5ec
minghli <minghli@cisco.com>
2020-01-10 14:18:40 +0800

    support parse fileList

--- src/bin/app.js
+++ src/bin/app.js
@@ -8,70 +8,70 @@ const MsgUtil = require('../utils/msgUtil');
 
 class App {
 
-    constructor() {
-        this.parse = new Parse();
-        this.msg = new MsgUtil();
-        this.initFunc();
-    }
+  constructor() {
+    this.parse = new Parse();
+    this.msg = new MsgUtil();
+    this.initFunc();
+  }
 
-    initFunc() {
-        program.version('0.1.0');
+  initFunc() {
+    program.version('0.1.0');
 
-        program.on('--help', () => {
-            console.log('\nExamples: \n$ opappo dc <fileName>\n$ opappo d  <fileName>');
-        });
+    program.on('--help', () => {
+      console.log('\nExamples: \n$ opappo dc <fileName>\n$ opappo d  <fileName>');
+    });
 
-        program
-            .command('dc [fileName]')
-            .alias('d')
-            .description('parse a DC file')
-            .option('-s, --select', 'Select a file')
-            .action((fileName, options) => {
-                if (options.select) {
-                    const fileList = this.parse.dirWalk(process.cwd());
-                    if (fileList.length === 0) {
-                        this.msg.info('There is no Excel file under the current path');
-                        process.exit();
-                    }
-                    const promps = [];
-                    promps.push({
-                        type: 'list',
-                        name: 'filePath',
-                        message: chalk.hex('#c0ca33')('Select a file'),
-                        choices: this.parse.dirWalk(process.cwd())
-                    });
-                    inquirer.prompt(promps).then(answers => {
-                        this.parse.parseDc(answers.filePath);
-                    });
-                } else {
-                    this.parse.getPath(fileName, 'dc');
-                }
-            });
+    program
+      .command('dc [fileName]')
+      .alias('d')
+      .description('parse a DC file')
+      .option('-s, --select', 'Select a file')
+      .action((fileName, options) => {
+        if (options.select) {
+          const fileList = this.parse.dirWalk(process.cwd());
+          if (fileList.length === 0) {
+            this.msg.info('There is no Excel file under the current path');
+            process.exit();
+          }
+          const promps = [];
+          promps.push({
+            type: 'list',
+            name: 'filePath',
+            message: chalk.hex('#c0ca33')('Select a file'),
+            choices: this.parse.dirWalk(process.cwd())
+          });
+          inquirer.prompt(promps).then(answers => {
+            this.parse.parseDc(answers.filePath);
+          });
+        } else {
+          this.parse.getPath(fileName, 'dc');
+        }
+      });
 
-        program
-            .command('feedback [fileName]')
-            .alias('f')
-            .description('parse a Feedback file')
-            .option('-s, --select', 'Select a file')
-            .action((fileName, options) => {
-                if (options.select) {
-                    const promps = [];
-                    promps.push({
-                        type: 'list',
-                        name: 'filePath',
-                        message: chalk.hex('#c0ca33')('Select a file'),
-                        choices: this.parse.dirWalk(process.cwd())
-                    });
-                    inquirer.prompt(promps).then(answers => {
-                        this.parse.parseFeedback(answers.filePath);
-                    });
-                } else {
-                    this.parse.getPath(fileName, 'feedback');
-                }
-            });
+    program
+      .command('feedback [fileName]')
+      .alias('f')
+      .description('parse a Feedback file')
+      .option('-s, --select', 'Select a file')
+      .action((fileName, options) => {
+        if (options.select) {
+          const promps = [];
+          promps.push({
+            type: 'list',
+            name: 'filePath',
+            message: chalk.hex('#c0ca33')('Select a file'),
+            choices: this.parse.dirWalk(process.cwd())
+          });
+          inquirer.prompt(promps).then(answers => {
+            this.parse.parseFeedback(answers.filePath);
+          });
+        } else {
+          this.parse.getPath(fileName, 'feedback');
+        }
+      });
 
-        program.parse(process.argv);
-    }
+    program.parse(process.argv);
+  }
 
 }
 
--- src/lib/parse.js
+++ src/lib/parse.js
@@ -7,106 +7,124 @@ const MsgUtil = require('../utils/msgUtil');
 
 class Parse {
 
-    constructor() {
-        this.msg = new MsgUtil();
+  constructor() {
+    this.msg = new MsgUtil();
+  }
+
+  test() {
+    console.log('!', this);
+  }
+
+  /**
+   * If there is no filename, the path is the current directory
+   * @param {string} fileName from input
+   * @param {string} type 'dc' or 'feedback'
+   */
+  getPath(fileName, type) {
+    let fileList = [];
+    if (fileName) {
+      // single file
+      if (this.isExcel(fileName)) {
+        fileList.push({
+          name: fileName,
+          value: `${process.cwd()}\\${fileName}`
+        });
+      } else {
+        this.msg.printErrByType('TypeError');
+        process.exit(1);
+      }
+    } else {
+      // files of the dir and sub dir
+      fileList = this.dirWalk(process.cwd());
     }
 
-    getPath(fileName, type) {
-        let path = null;
-
-        if (fileName) {
-            // check file ext
-            if (!this.isExcel(fileName)) {
-                this.msg.printErrByType('TypeError');
-                process.exit(1);
-            }
-            path = `${process.cwd()}\\${fileName}`;
-        } else {
-            path = `${process.cwd()}`;
-        }
-
-        if (type === 'dc') {
-            this.parseDc(path);
-        }
-        if (type === 'feedback') {
-            this.parseFeedback(path);
-        }
+    if (type === 'dc') {
+      _.each(fileList, obj => {
+        this.parseDc(obj.value);
+      });
     }
-
-    parseDc(filePath) {
-        this.msg.info(`parse dc: ${filePath}`);
-        try {
-            const workbook = XLSX.readFile(filePath);
-            const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-            // calc number
-            const totalNum = worksheet['B2'].v;
-            const missNum = worksheet['C2'].v ? worksheet['C2'].v : 0;
-            const adviceNum = worksheet['F2'].v ? worksheet['F2'].v : 0;
-            const dcNum = missNum + adviceNum;
-            this.msg.boxMsg(`Total: ${totalNum}\nMiss Count: ${missNum}\nAdvice Count: ${adviceNum}\n=================\nDC Count: ${dcNum}\n\n${dcNum === 0 ? 'good!' : ''} `);
-
-            this.getStaff(filePath);
-        } catch (err) {
-            this.msg.printErrByType(err.code);
-        }
+    if (type === 'feedback') {
+      _.each(fileList, obj => {
+        this.parseFeedback(obj.value);
+      });
     }
+  }
 
-    parseFeedback(filePath) {
-        if (filePath) {
-            this.msg.info('parse feedback: ', filePath);
-        }
+  parseDc(filePath) {
+    console.log('!!!this: ', this);
+    this.msg.info(`parse dc: ${filePath}`);
+    try {
+      const workbook = XLSX.readFile(filePath);
+      const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+      // calc number
+      const totalNum = worksheet['B2'].v;
+      const missNum = worksheet['C2'].v ? worksheet['C2'].v : 0;
+      const adviceNum = worksheet['F2'].v ? worksheet['F2'].v : 0;
+      const dcNum = missNum + adviceNum;
+      this.msg.boxMsg(`Total: ${totalNum}\nMiss Count: ${missNum}\nAdvice Count: ${adviceNum}\n=================\nDC Count: ${dcNum}\n\n${dcNum === 0 ? 'good!' : ''} `);
+
+      this.getStaff(filePath);
+    } catch (err) {
+      this.msg.printErrByType(err.code);
     }
+  }
 
-    /**
-    * Check the name ext 
-    * @param {string} str name to be check
-    */
-    isExcel(str) {
-        const extReg = new RegExp('(.+?).xlsx*$', 'gi');
-        return str.match(extReg) ? true : false;
+  parseFeedback(filePath) {
+    if (filePath) {
+      this.msg.info('parse feedback: ', filePath);
     }
+  }
 
-    /**
-     * walk the dir
-     * @param {string} dirPath dir path to walk
-     */
-    dirWalk(dirPath, list) {
-        const res = list ? list : [];
-        try {
-            const files = fs.readdirSync(dirPath);
-            files.forEach((fileName) => {
-                const filePath = path.join(dirPath, fileName);
-                const stats = fs.statSync(filePath);
-                if (stats.isFile()) {
-                    if (this.isExcel(fileName)) {
-                        res.push({
-                            name: chalk.hex('#f44336')(fileName),
-                            value: filePath
-                        });
-                    }
-                }
-                if (stats.isDirectory()) {
-                    this.dirWalk(filePath, res);
-                }
+  /**
+  * Check the name ext 
+  * @param {string} str name to be check
+  */
+  isExcel(str) {
+    const extReg = new RegExp('(.+?).xlsx*$', 'gi');
+    return str.match(extReg) ? true : false;
+  }
+
+  /**
+   * walk the dir
+   * @param {string} dirPath dir path to walk
+   */
+  dirWalk(dirPath, list) {
+    const res = list || [];
+    try {
+      const files = fs.readdirSync(dirPath);
+      files.forEach((fileName) => {
+        const filePath = path.join(dirPath, fileName);
+        const stats = fs.statSync(filePath);
+        if (stats.isFile()) {
+          if (this.isExcel(fileName)) {
+            res.push({
+              name: chalk.hex('#f44336')(fileName),
+              value: filePath
             });
-        } catch (err) {
-            if (_.startsWith(err, 'Error: ')) {
-                this.msg.printErrByType(_.split(err, ':')[1].trim());
-            }
-            this.msg.error(err);
+          }
         }
-
-        return res;
+        if (stats.isDirectory()) {
+          this.dirWalk(filePath, res);
+        }
+      });
+    } catch (err) {
+      if (_.startsWith(err, 'Error: ')) {
+        this.msg.printErrByType(_.split(err, ':')[1].trim());
+      }
+      this.msg.error(err);
     }
 
-    getStaff(filePath) {
-        const fileName = _.last(filePath.split('\\')) || _.last(filePath.split('/'));
-        const staffReg = new RegExp('_(?<a>[0-9a-zA-Z]+?)_*for_*(?<b>[0-9a-zA-Z]+?)_', 'i');
-        const matchObj = staffReg.exec(fileName);
-        if (matchObj) {
-            this.msg.info(`Staff: ${matchObj.groups.a} ==> ${matchObj.groups.b}`);
-        }
+    return res;
+  }
+
+  getStaff(filePath) {
+    const fileName = _.last(filePath.split('\\')) || _.last(filePath.split('/'));
+    const staffReg = new RegExp('_(?<a>[0-9a-zA-Z]+?)_*for_*(?<b>[0-9a-zA-Z]+?)_', 'i');
+    const matchObj = staffReg.exec(fileName);
+    if (matchObj) {
+      this.msg.info(`Staff: ${matchObj.groups.a} ==> ${matchObj.groups.b}`);
     }
+  }
 }
 
 module.exports = Parse;
